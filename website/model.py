597eb3db807e175a785b063a81b392c376b7149a
bingdong45 <bdong45@wisc.edu>
2024-11-17 02:43:41 -0600

    Create model.py

---
+++ website/model.py
@@ -0,0 +1,51 @@
+import torch
+from torch import nn
+import numpy as np
+import scipy
+from torch.nn import functional as F
+
+class GoogleNet(nn.Module):
+    def __init__(self):
+        super(GoogleNet, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            # Additional layers
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self._to_linear = None
+        self.calculate_linear_input_size()
+        self.fc_layers = nn.Sequential(
+            nn.Linear(self._to_linear, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 10)
+        )
+
+    def calculate_linear_input_size(self):
+        with torch.no_grad():
+            x = torch.randn(1, 3, 250, 250)
+            x = self.conv_layers(x)
+            self._to_linear = x.view(1, -1).size(1)
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x)
+        return x
