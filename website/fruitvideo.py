03f2e91805dbe418cc3e61aaf05c156a15c716c6
bingdong45 <bdong45@wisc.edu>
2024-11-17 02:42:46 -0600

    Update fruitvideo.py

--- website/fruitvideo.py
+++ website/fruitvideo.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import torch
 import numpy as np
+from model import GoogleNet
+from torchvision import transforms
+import torch.nn.functional as F
+import io
+from PIL import Image
 
 st.title("_What's that_ :green[Fruit] :apple:")
 st.header(":camera: Mode")
@@ -11,19 +16,47 @@ multi = '''
 3. Click “Search” to see your results!  
 '''
 st.markdown(multi)
+
 img_file_buffer = st.camera_input("Take a picture here")
 
 if img_file_buffer is not None:
-    # To read image file buffer as a 3D uint8 tensor with PyTorch:
-    bytes_data = img_file_buffer.getvalue()
-    torch_img = torch.ops.image.decode_image(
-        torch.from_numpy(np.frombuffer(bytes_data, np.uint8)), 3
-    )
+    try:
 
-    # Check the type of torch_img:
-    # Should output: <class 'torch.Tensor'>
-    st.write(type(torch_img))
+        bytes_data = img_file_buffer.getvalue()
+        image = Image.open(io.BytesIO(bytes_data)).convert('RGB')
 
-    # Check the shape of torch_img:
-    # Should output shape: torch.Size([channels, height, width])
-    st.write(torch_img.shape)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+ 
+        transform = transforms.Compose([
+            transforms.Resize((250, 250)), 
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+
+        torch_img = transform(image)
+        torch_img = torch_img.unsqueeze(0)  # Add batch dimension
+
+
+        model = GoogleNet()
+        state_dict = torch.load('./model_weight.pth', map_location=torch.device('cpu'))
+        model.load_state_dict(state_dict)
+
+        model.eval()
+
+
+        with torch.no_grad():
+            result = model(torch_img)
+            predicted_class = torch.argmax(result, dim=1).item()
+            labels = ['Apple', 'Avocado', 'Banana', 'Cherry', 'Kiwi', 'Mango', 'Orange', 'Pineapple', 'Strawberries', 'Watermelon']
+            fruit = labels[predicted_class]
+            st.write(f"Predicted Fruit: {fruit}")
+
+        # st.write(f"Tensor Type: {type(torch_img)}")
+        # st.write(f"Tensor Shape: {torch_img.shape}")
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
