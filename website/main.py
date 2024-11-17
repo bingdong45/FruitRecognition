d67963a877b2dda80ebcc62c79ba7e52588182c0
bingdong45 <bdong45@wisc.edu>
2024-11-17 02:43:25 -0600

    Update main.py

--- website/main.py
+++ website/main.py
@@ -5,7 +5,6 @@ pages = {
     "What's that fruit?": [
         st.Page("fruitvideo.py", title="Take Picture"),
         st.Page("fruitimage.py", title="Upload image"),
-        st.Page("fruitdraw.py", title="Draw image")
     ],
     "Resources": [
         st.Page("About.py", title="About us"),
